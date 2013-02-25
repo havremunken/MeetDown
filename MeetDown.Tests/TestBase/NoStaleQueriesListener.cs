7ef923fab519af2936a91538e634f6801f0ac7af
Rune Jacobsen <havremunken@gmail.com>
2013-02-25 23:01:33 +0100

    Describe NoStaleQueriesListener in XML doc

--- MeetDown.Tests/TestBase/NoStaleQueriesListener.cs
+++ MeetDown.Tests/TestBase/NoStaleQueriesListener.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace MeetDown.Tests.TestBase
 {
+    /// <summary>
+    /// A class used to configure the embeddable raven store to never return stale results
+    /// </summary>
     public class NoStaleQueriesListener : IDocumentQueryListener
     {
         public void BeforeQueryExecuted(IDocumentQueryCustomization queryCustomization)
