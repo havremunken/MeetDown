d2bf59dcea6c6c02bf602d821c842b7b200f588d
Rune Jacobsen <havremunken@gmail.com>
2013-03-10 07:47:24 +0100

    Create HttpStubs for route testing + tests

---
+++ MeetDown.Tests/Web/App_Start/RouteConfigTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+using MeetDown.Tests.TestBase;
+using MeetDown.Web;
+using Xunit;
+using System.Web.Mvc;
+
+namespace MeetDown.Tests.Web.App_Start
+{
+    public class RouteConfigTests
+    {
+        #region Setting up route context etc.
+
+        private StubHttpContextForRouting _context;
+        private RouteCollection _routes;
+
+        private void SetupRouteRequest(string requestUrl)
+        {
+            if (requestUrl.StartsWith("/"))
+                requestUrl = "~" + requestUrl;
+            else if (!requestUrl.StartsWith("~/"))
+                requestUrl = "~/" + requestUrl;
+
+            _context = new StubHttpContextForRouting(requestUrl: requestUrl);
+            _routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(_routes);
+        }
+
+        private RouteData GetRouteData()
+        {
+            return _routes.GetRouteData(_context);
+        }
+
+        #endregion
+
+        #region Group route tests
+
+        [Fact]
+        public void RouteConfig_BasicGroupRoute_Works()
+        {
+            // Arrange
+            SetupRouteRequest("DrammenSoftwareDeveloper");
+
+            // Act
+            var routeData = GetRouteData();
+
+            // Assert
+            Assert.NotNull(routeData);
+            Assert.Equal("Group", routeData.Values["controller"]);
+            Assert.Equal("Info", routeData.Values["action"]);
+            Assert.Equal(UrlParameter.Optional, routeData.Values["id"]);
+            Assert.Equal("DrammenSoftwareDeveloper", routeData.Values["slug"]);
+        }
+
+        [Fact]
+        public void RouteConfig_GroupEventRoute_Works()
+        {
+            // Arrange
+            SetupRouteRequest("DrammenSoftwareDeveloper/events/1234");
+
+            // Act
+            var routeData = GetRouteData();
+
+            // Assert
+            Assert.NotNull(routeData);
+            Assert.Equal("Group", routeData.Values["controller"]);
+            Assert.Equal("events", routeData.Values["action"]);
+            Assert.Equal("1234", routeData.Values["id"]);
+            Assert.Equal("DrammenSoftwareDeveloper", routeData.Values["slug"]);
+        }
+
+        [Fact]
+        public void RouteConfig_GroupPastEventsRoute_Works()
+        {
+            // Arrange
+            SetupRouteRequest("DrammenSoftwareDeveloper/events/past");
+
+            // Act
+            var routeData = GetRouteData();
+
+            // Assert
+            Assert.NotNull(routeData);
+            Assert.Equal("Group", routeData.Values["controller"]);
+            Assert.Equal("events", routeData.Values["action"]);
+            Assert.Equal("past", routeData.Values["id"]);
+            Assert.Equal("DrammenSoftwareDeveloper", routeData.Values["slug"]);
+        }
+
+        [Fact]
+        public void RouteConfig_GroupMemberRoute_Works()
+        {
+            // Arrange
+            SetupRouteRequest("DrammenSoftwareDeveloper/members/1234");
+
+            // Act
+            var routeData = GetRouteData();
+
+            // Assert
+            Assert.NotNull(routeData);
+            Assert.Equal("Group", routeData.Values["controller"]);
+            Assert.Equal("members", routeData.Values["action"]);
+            Assert.Equal("1234", routeData.Values["id"]);
+            Assert.Equal("DrammenSoftwareDeveloper", routeData.Values["slug"]);
+        }
+
+        #endregion
+    }
+}
--- MeetDown.Web/App_Start/RouteConfig.cs
+++ MeetDown.Web/App_Start/RouteConfig.cs
@@ -13,11 +13,17 @@ namespace MeetDown.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "GroupRoute",
+                url: "{slug}/{action}/{id}",
+                defaults: new {controller = "Group", action = "Info", id = UrlParameter.Optional}
+                );
         }
     }
 }
