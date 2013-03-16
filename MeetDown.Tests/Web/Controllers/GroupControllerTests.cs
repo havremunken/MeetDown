7712916c5b07eddce605c2c1ac6ae7feaf55217f
Rune Jacobsen <havremunken@gmail.com>
2013-03-16 12:03:21 +0100

    Add GroupController, tests and views

---
+++ MeetDown.Tests/Web/Controllers/GroupControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using MeetDown.Core.Entities;
+using MeetDown.Tests.TestBase;
+using MeetDown.Web.Controllers;
+using System.Web.Mvc;
+using Xunit;
+
+namespace MeetDown.Tests.Web.Controllers
+{
+    public class GroupControllerTests : RavenDbTest
+    {
+        #region Info tests
+
+        [Fact]
+        public void Info_WhenCalledWithEmptyString_RedirectsToHomeIndex()
+        {
+            // Arrange
+            var session = DocumentStore.OpenSession();
+            var sut = new GroupController(session);
+
+            // Act
+            var result = sut.Info(string.Empty);
+
+            // Assert
+            var redirectResult = result as RedirectToRouteResult;
+            Assert.NotNull(redirectResult);
+            Assert.Equal(redirectResult.RouteValues["action"], "Index");
+            Assert.Equal(redirectResult.RouteValues["controller"], "Home");
+        }
+
+        [Fact]
+        public void Info_WhenGroupDoesNotExist_ReturnsUnknownGroupView()
+        {
+            // Arrange
+            var session = DocumentStore.OpenSession();
+            var sut = new GroupController(session);
+
+            // Act
+            var result = sut.Info("NonExistingGroup") as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("UnknownGroup", result.ViewName);
+        }
+
+        [Fact]
+        public void Info_WhenGroupExists_ReturnsViewWithCorrectModel()
+        {
+            // Arrange
+            var session = DocumentStore.OpenSession();
+            var testGroup = new Group
+                {
+                    Name = "TestGroup",
+                };
+            session.Store(testGroup);
+            session.SaveChanges();
+
+            var sut = new GroupController(session);
+
+            // Act
+            var result = sut.Info("TestGroup") as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Same(testGroup, result.Model);
+            Assert.Equal("Info", result.ViewName);
+        }
+
+        #endregion
+    }
+}
---
+++ MeetDown.Web/Controllers/GroupController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Raven.Client;
+using MeetDown.Core.Entities;
+
+namespace MeetDown.Web.Controllers
+{
+    public class GroupController : Controller
+    {
+        #region Fields
+
+        private IDocumentSession _session;
+
+        #endregion
+
+        #region Constructor
+
+        public GroupController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        #endregion
+
+        public ActionResult Info(string id)
+        {
+            if (String.IsNullOrWhiteSpace(id))
+                return RedirectToAction("Index", "Home");
+
+            var grp = _session.Query<Group>()
+                                .SingleOrDefault(g => g.Slug == id);
+
+            return grp == null ? View("UnknownGroup") : View("Info", grp);
+        }
+
+    }
+}
