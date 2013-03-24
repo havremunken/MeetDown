8c9b7baa668094c76ead3c9eda643ee85d68beb8
Rune Jacobsen <havremunken@gmail.com>
2013-03-24 13:31:45 +0100

    Create route for non-group actions/controllers

--- MeetDown.Web/App_Start/RouteConfig.cs
+++ MeetDown.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace MeetDown.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "NonGroup",
+                url: "{controller}/{action}/{id}",
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
+                constraints: new {controller = @"home|account|seed"}
+                );
+
             //routes.MapRoute(
             //    name: "Default",
             //    url: "{controller}/{action}/{id}",
