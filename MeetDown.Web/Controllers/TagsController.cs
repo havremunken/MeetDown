46bfcadb8201e29e765fea3ed379acb93df3b169
Rune Jacobsen <havremunken@gmail.com>
2013-03-26 22:01:04 +0100

    Add TagsController for tag search

---
+++ MeetDown.Web/Controllers/TagsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeetDown.Core.Entities;
+using MeetDown.Web.Models;
+using Raven.Client;
+
+namespace MeetDown.Web.Controllers
+{
+    public class TagsController : Controller
+    {
+        #region Fields
+
+        private IDocumentSession _session;
+
+        #endregion
+
+        #region Constructor
+
+        public TagsController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        #endregion
+        
+        public ActionResult Find(string id)
+        {
+            var model = new TagSearchModel
+                {
+                    Tag = id,
+                    Groups = _session.Query<Group>()
+                                     .Where(g => g.Tags.Contains(id))
+                                     .ToList()
+                };
+
+            return View(model);
+        }
+    }
+}
---
+++ MeetDown.Web/Models/TagSearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MeetDown.Core.Entities;
+
+namespace MeetDown.Web.Models
+{
+    public class TagSearchModel
+    {
+        public string Tag { get; set; }
+        public IEnumerable<Group> Groups { get; set; }
+    }
+}
