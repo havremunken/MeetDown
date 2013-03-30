6bef36429546de57de43954a9611174ec05c1aef
Rune Jacobsen <havremunken@gmail.com>
2013-03-30 12:00:43 +0100

    Remove default MVC route config

--- MeetDown.Web/App_Start/RouteConfig.cs
+++ MeetDown.Web/App_Start/RouteConfig.cs
@@ -12,25 +12,6 @@ namespace MeetDown.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "NonGroup",
-                url: "{controller}/{action}/{id}",
-                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
-                constraints: new {controller = @"home|account|seed|tags"}
-                );
-
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
-
-            //routes.MapRoute(
-            //    name: "GroupRoute",
-            //    url: "{slug}/{action}/{id}",
-            //    defaults: new {controller = "Group", action = "Info", id = UrlParameter.Optional}
-            //    );
         }
     }
 }
